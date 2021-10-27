0ea025211e8a38588acb57792a3d856d5f917ecb
Kyle Grenier <kyle.grenier11@gmail.com>
2021-10-26 23:54:10 -0500

    Solid state now uses gas camera for effect

--- Assets/Scripts/States of Matter/Concrete States/SolidState.cs
+++ Assets/Scripts/States of Matter/Concrete States/SolidState.cs
@@ -2,7 +2,6 @@
 *    Author: Kyle Grenier
 *    Contributors: 
 *    Date Created: 
-*    Brief Description: 
 *******************************************************************/
 using UnityEngine;
 
@@ -10,15 +9,57 @@ namespace GoofyGhosts
 {
     public class SolidState : IMatterState
     {
-        public override void OnGrounded()
-        {
-            base.OnGrounded();
-            // TODO: Camera shake.
-        }
+        /// <summary>
+        /// Reference to the gas camera.
+        /// </summary>
+        /// <remarks>Solid state uses this camera just like the gas state.</remarks>
+        private GameObject gasCamera;
+        /// <summary>
+        /// Reference to the movement camera.
+        /// </summary>
+        private GameObject movementCamera;
 
+        #region -- // Init // --
+        /// <summary>
+        /// Finding cameras.
+        /// </summary>
+        private void Start()
+        {
+            gasCamera = GameObject.FindGameObjectWithTag("GasCamera");
+            if (gasCamera == null)
+            {
+                Debug.LogWarning("[SolidState]: Could not find gas camera. Tag camera 'GasCamera'.");
+            }
+
+            movementCamera = GameObject.FindGameObjectWithTag("MovementCamera");
+            if (movementCamera == null)
+            {
+                Debug.LogWarning("[SolidState]: Could not find GameObject tagged 'MovementCamera'.");
+            }
+        }
+        #endregion
+
+        #region -- // Activation / Deactivation // --
         public override void Activate(StateSwapper swapper)
         {
             base.Activate(swapper);
+            movementCamera.SetActive(false);
+            gasCamera.SetActive(true);
+
+            // TODO: Camera shake.
+        }
+
+        public override void Deactivate()
+        {
+            base.Deactivate();
+            movementCamera.SetActive(true);
+            gasCamera.SetActive(false);
+        }
+        #endregion
+
+        public override void OnGrounded()
+        {
+            base.OnGrounded();
             // TODO: Camera shake.
         }
 
