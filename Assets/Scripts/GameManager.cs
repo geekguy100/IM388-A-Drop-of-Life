fdcad1f3e69ce01704bf692cfcaa58b2bfed081c
Kyle Grenier <42879674+geekguy100@users.noreply.github.com>
2021-10-27 13:36:04 -0500

    Update GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -21,8 +21,8 @@ namespace GoofyGhosts
 
         private void OnEnable()
         {
-           // controls.Settings.Reload.performed += _ => SceneManager.LoadScene(1);
-         //   controls.Settings.Menu.performed += _ => { Cursor.lockState = CursorLockMode.None; Cursor.visible = true; SceneManager.LoadScene(0); };
+            controls.Settings.Reload.performed += _ => SceneManager.LoadScene(1);
+            controls.Settings.Menu.performed += _ => { Cursor.lockState = CursorLockMode.None; Cursor.visible = true; SceneManager.LoadScene(0); };
 
             controls.Settings.Enable();
         }
