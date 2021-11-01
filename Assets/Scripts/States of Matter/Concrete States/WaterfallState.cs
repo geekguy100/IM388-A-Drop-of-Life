7e64b82782b2d61e07270d7080852a83dc615ff4
Kyle Grenier <kyle.grenier11@gmail.com>
2021-10-31 20:01:04 -0500

    BUG FIX: Player occasionally swapped out of waterfall state, similar to the previous liquid state bug, while entering it.
    Fixed by: updating the time the waterfall state's canSwap boolean is set to true in WaterfallState.cs

--- Assets/Scripts/States of Matter/Concrete States/WaterfallState.cs
+++ Assets/Scripts/States of Matter/Concrete States/WaterfallState.cs
@@ -43,7 +43,7 @@ namespace GoofyGhosts
             StartCoroutine(WaitThenEnable());
             IEnumerator WaitThenEnable()
             {
-                yield return new WaitForSeconds(0.01f);
+                yield return new WaitForSeconds(0.5f);
                 canSwap = true;
             }
         }
